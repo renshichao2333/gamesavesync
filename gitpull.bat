4f1bbc8ca7481756ce1d0acf8af95574ad64fdc4
chaooren <chaooren@tencent.com>
2023-04-25 14:30:47 +0800

    Öܶþ/04/25-14:30:.91

---
+++ gitpull.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+set git_repo=https://github.com/renshichao2333/gamesavesync.git
+set git_branch=master
+set git_folder=%cd%
+echo Cloning %git_repo%...
+git clone --branch %git_branch% %git_repo% %git_folder%
+
+echo Updating %git_branch% branch...
+cd %git_folder%
+git pull origin %git_branch%
+
+echo Done.
---
+++ gitpush.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+set git_repo=https://github.com/renshichao2333/gamesavesync.git
+set git_branch=master
+
+echo Adding changes...
+git add .
+
+echo Committing changes...
+
+for /f "tokens=1-4 delims=/ " %%a in ('date /t') do (
+    set year=%%d
+    set month=%%b
+    set day=%%c
+)
+
+for /f "tokens=1-3 delims=:." %%a in ('time /t') do (
+    set hour=%%a
+    set minute=%%b
+    set second=%%c
+    set millisecond=%time:~9,2%
+)
+
+set datetime=%year%/%month%/%day%-%hour%:%minute%:%second%.%millisecond%
+
+echo %datetime% > .git\COMMIT_EDITMSG
+git commit -F .git\COMMIT_EDITMSG
+
+echo Pushing changes to %git_branch% branch...
+git push origin %git_branch%
+
+echo Done.
